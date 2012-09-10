bd59a84fe99085d4e4afc8fa9e1614747e8fe4e9
Toshiaki Asai <toshi@toshia-mac.(none)>
2012-09-10 20:39:19 +0900

    mikutter 0.2 対応

--- call_api_fav.rb
+++ call_api_fav.rb
@@ -1,9 +1,5 @@
 # -*- coding:utf-8 -*-
 
-miquire :mui, 'skin'
-miquire :addon, 'addon'
-miquire :addon, 'settings'
-
 #遅延ふぁぼを有効(true)|無効(false)にします
 #有効にするとmikutterが重くなる可能性があります
 $is_sleep = false
@@ -18,17 +14,19 @@ def xorshift128_sleep(stime)
   sleep(rand(stime.to_i)+ (w%10).to_i)
 end
 
-Module.new do
-  
-  plugin = Plugin::create(:call_api_Tofav)
-  
-  main = Gtk::TimeLine.new()
-  service = nil
-  
+Plugin.create(:call_api_fav) do
+
   querybox = Gtk::Entry.new()
-  querycont = Gtk::VBox.new(false, 0)
   searchbtn = Gtk::Button.new('ふぁぼ候補')
-  
+
+  tab(:call_api_fav, 'Call_Api_ToFav') do
+    set_icon MUI::Skin.get("etc.png")
+    shrink
+    nativewidget Gtk::HBox.new(false, 0).pack_start(querybox).closeup(searchbtn)
+    expand
+    timeline :call_api_fav
+  end
+
   searchbtn.signal_connect('clicked'){ |elm|
     favnum = 10
     stime = 1
@@ -46,18 +44,16 @@ Module.new do
       #読み込みが失敗したら1〜10秒の遅延で10個だけふぁぼるよ
     end
     
-    main.clear
+    timeline(:call_api_fav).clear
     #テキストボックスが空なら何もしないよ
     if querybox.text.size > 0 then
       screen_name = querybox.text
       user = User.findbyidname("#{screen_name}", true)
       user[:id] if user
-      service.call_api(:user_timeline, :user_id => user[:id],
-                       :no_auto_since_id => true,
-                       :count => favnum.to_i){ |res|
-        Delayer.new{
-          main.add(res)
-        }
+      Service.primary.call_api(:user_timeline, :user_id => user[:id],
+                               :no_auto_since_id => true,
+                               :count => favnum.to_i){ |res|
+        timeline(:call_api_fav) << res
         res.each do |mes|
           unless mes.favorite? || mes.retweet?
             if $is_sleep == true then
@@ -79,17 +75,4 @@ Module.new do
       }
     end
   }
-  
-  querycont.closeup(Gtk::HBox.new(false, 0).pack_start(querybox).closeup(searchbtn))
-  
-  plugin.add_event(:boot){ |s|
-    service = s
-    container = Gtk::VBox.new(false, 0).pack_start(querycont, false).pack_start(main, true)
-    Plugin.call(:mui_tab_regist, container, 'Call_Api_ToFav', MUI::Skin.get("etc.png"))
-    #同梱のtarget.pngをskin/data
-    #に置いた時は上をコメントアウトしてこちらをお使いください
-    #Plugin.call(:mui_tab_regist, container, 'Call_Api_ToFav', MUI::Skin.get("target.png"))
-    
-  }
-  
 end
